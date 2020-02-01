7634fdc9b7f8f994cf1cdeff1784bc5e35ae6112
Eduards <webcooking.pro@gmail.com>
2020-02-01 18:41:32 +0200

    Add js code

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.querySelector('#count');
+const total = document.querySelector('#total');
+const movieSelect = document.querySelector('#movie');
+
+let ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+
+}
+
+//Movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  console.log(ticketPrice);
+
+  updateSelectedCount();
+})
+//Seat click event
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+
+  }
+
+})
